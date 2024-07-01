e8a40498219c63e6a5ed93a20243df639cf42b22
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-07-01 13:07:46 +0100

    Update Index.js

--- Routes/Index.js
+++ Routes/Index.js
@@ -3,6 +3,8 @@ const router=express.Router()
 const {SigninController,LoginController,ForgotPasswordController,UpdateProfilePictureController,RememberMeController} =require('../Controllers/User')
 const {AddCourseController,GetAllCourseController,DeleteCourseController,AddMultipleCourseController} =require('../Controllers/Course')
 
+
+router.get('/','we are live !!!!!!1')
 router.post('/signin',SigninController)
 router.post('/login',LoginController)
 router.get('/rememberMe',RememberMeController)
