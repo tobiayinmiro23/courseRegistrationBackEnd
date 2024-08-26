ba56debdebb67cc994ac7578ef338a97f6f2b215
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-08-26 12:37:57 +0100

    Update Index.js

--- Routes/Index.js
+++ Routes/Index.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const router=express.Router()
-const {SigninController,LoginController,ForgotPasswordController,UpdateProfilePictureController,RememberMeController} =require('../Controllers/User')
-const {AddCourseController,GetAllCourseController,DeleteCourseController,AddMultipleCourseController} =require('../Controllers/Course')
+const router = express.Router()
+const { SigninController, LoginController, GetPhotoController, ForgotPasswordController, UpdateProfilePictureController } = require('../Controllers/User')
+const { AddCourseController, GetAllCourseController, DeleteCourseController, AddMultipleCourseController } = require('../Controllers/Course')
 
-
-router.get('/',(req,res)=>res.send('<h1>we are live !!!!!!</h1>'))
-router.post('/signin',SigninController)
-router.post('/login',LoginController)
-router.get('/rememberMe',RememberMeController)
-router.post('/addCourse',AddCourseController)
-router.post('/addMultipleCourse',AddMultipleCourseController)
-router.post('/allCourse',GetAllCourseController)
-router.delete('/deleteCourse/:id',DeleteCourseController)
-router.post('/forgotPassword',ForgotPasswordController)
-router.post('/updateProfilePicture',UpdateProfilePictureController)
+router.get('/', (req, res) => res.send('<h1>we are live !!!!!!</h1>'))
+router.post('/signin', SigninController)
+router.post('/login', LoginController)
+router.post('/getPhoto', GetPhotoController)
+// router.get('/rememberMe',RememberMeController)
+router.post('/addCourse', AddCourseController)
+router.post('/addMultipleCourse', AddMultipleCourseController)
+router.post('/allCourse', GetAllCourseController)
+router.delete('/deleteCourse/:id', DeleteCourseController)
+router.post('/forgotPassword', ForgotPasswordController)
+router.post('/updateProfilePicture', UpdateProfilePictureController)
 
 module.exports = router
