41684488aaccbada806443b18fe727d4123c008e
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-09-03 14:55:24 +0100

    Update Authorization.js

--- HelperFunction/Authorization.js
+++ HelperFunction/Authorization.js
@@ -4,20 +4,13 @@ const jwt = require('jsonwebtoken')
 const verifyToken= async(id,token)=>{
         try{
             let userExists=await User.findById(id)
-         console.log(userExists,'userExists')
-          let valid = jwt.verify(token, process.env.SECRETEKEY)
-          console.log(valid,'valid')
-          console.log(process.env.SECRETEKEY)
             if(userExists?._id){
                 let valid = jwt.verify(token, process.env.SECRETEKEY)
-                console.log(valid,'valid')
-                console.log(process.env.SECRETEKEY)
-                if(valid===userExists.userName)return true
-                 return 'unauthorised'
+                 if (valid === userExists?.userName) return true
+                 return 'you are not authorised to access this route'
             }
                 return 'cannot acces this route user does not exist'
         }catch(err){
-            console.log(err)
             if(err.name === 'JsonWebTokenError') return err.message
             if(err.name === 'CastError') return 'you are not authorized to access this route'
             return err?.message || err
