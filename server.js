dab5890af46f9b1c3f7f40a736ecdbe5959de909
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-07-12 16:39:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@tobi.
 
 const app=express()
 
-var whitelist =['https://school-portal-ml9i.onrender.com','https://tobiayinmiro23.github.io/portfolio/'];
+var whitelist =['https://school-portal-zjna.onrender.com','https://tobiayinmiro23.github.io/portfolio/'];
 // app.use(cors({credentials: true, origin: whitelist }));
 app.use(cors());
 app.use(express.json())
