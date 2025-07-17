e462b27be8d50f2f5146bbf837bfb85299c6bf25
Ayinmiro tobi <ayinmirotobi@gmail.com>
2025-07-17 14:56:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@tobi.
 
 const app=express()
 
-// app.use(cors({credentials: true, origin: 'https://school-portal-zjna.onrender.com' }));
-app.use(cors());
+app.use(cors({credentials: true, origin: 'https://school-portal-zjna.onrender.com' }));
 app.use(express.json())
 app.use(router)
 
