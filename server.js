293d420e20492caac2c9bcce43ce48ba17010c3f
Ayinmiro tobi <ayinmirotobi@gmail.com>
2026-01-08 19:56:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,30 @@ const router= require('./Routes/Index')
 const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@tobi.z8plmj1.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 
 const app=express()
-const corsOptions = {
-  origin: ['https://school-portal-zjna.onrender.com','https://tobiayinmiro23.github.io'],
-};
-app.use(cors({credentials: true,corsOptions }));
+// const corsOptions = {
+//   origin: ['https://school-portal-zjna.onrender.com','https://tobiayinmiro23.github.io'],
+// };
+// app.use(cors({credentials: true,corsOptions }));
+const allowedOrigins = [
+  "https://school-portal-zjna.onrender.com",
+  "https://tobiayinmiro23.github.io",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json())
 app.use(router)
 
@@ -19,4 +39,5 @@ async function main() {
 }
 
 app.listen(process.env.PORT || 3000,()=>main().catch((err) => console.log(err)))
+
 
